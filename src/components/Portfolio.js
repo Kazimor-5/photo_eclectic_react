f050fb5ae371cf206bb50d560a4281f23d3b7786
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-05-25 14:06:06 +0200

    adding categories array, renaming photo on assets folder

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,7 +3,7 @@ import React from 'react';
 import '../styles/Portfolio.css';
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return <section className='section-portfolio'>Portfolio</section>;
 };
 
 export default Portfolio;
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const navLinks = [
     id: 1,
     name: 'logo',
     anchor: '/',
-    src: './assets/logo.jpg',
+    src: './assets/logo.png',
   },
   {
     id: 2,
@@ -55,7 +55,336 @@ export const citations = [
   },
   {
     id: 3,
-    title: 'la photographie est notre memoire',
+    title: 'la photographie est notre mémoire',
     text: "La photographie est une sorte d'illusion de la réalité, même si c'est peut-être la représentation la plus fidèle. Les mythes contribuent à leur donner une interprétation. Mon propre souvenir est capturé au moment meme ou je prend la photo, c'est finalement l'appareil photo qui me sert de mémoire.",
+  },
+];
+
+export const categories = [
+  {
+    abstrait: [
+      {
+        id: 1,
+        name: 'oeufs',
+        src: './assets/abstrait/oeufs.jpg',
+      },
+      {
+        id: 2,
+        name: 'fleurs',
+        src: './assets/abstrait/fleurs.jpg',
+      },
+      {
+        id: 3,
+        name: 'fraises',
+        src: './assets/abstrait/fraises.jpg',
+      },
+      {
+        id: 4,
+        name: 'voiture',
+        src: './assets/abstrait/voiture.jpg',
+      },
+      {
+        id: 5,
+        name: 'sapin',
+        src: './assets/abstrait/sapin.jpg',
+      },
+      {
+        id: 6,
+        name: 'copeaux',
+        src: './assets/abstrait/copeaux.jpg',
+      },
+      {
+        id: 7,
+        name: 'copeaux',
+        src: './assets/abstrait/copeaux.jpg',
+      },
+      {
+        id: 8,
+        name: 'fleurs blanches',
+        src: './assets/abstrait/fleurs_blanches.jpg',
+      },
+    ],
+    animaux: [
+      {
+        id: 1,
+        name: 'chiens',
+        src: './assets/animaux/chiens.jpg',
+      },
+      {
+        id: 2,
+        name: 'chat',
+        src: './assets/animaux/chat.jpg',
+      },
+      {
+        id: 3,
+        name: 'chat',
+        src: './assets/animaux/chat2.jpg',
+      },
+    ],
+    collection: [
+      {
+        id: 1,
+        name: 'body',
+        src: './assets/collection/body.jpg',
+      },
+      {
+        id: 2,
+        name: 'body',
+        src: './assets/collection/body2.jpg',
+      },
+      {
+        id: 3,
+        name: 'body coté',
+        src: './assets/collection/body_cote.jpg',
+      },
+      {
+        id: 2,
+        name: 'body coté',
+        src: './assets/collection/body_cote2.jpg',
+      },
+    ],
+    mariage: [
+      {
+        id: 1,
+        name: 'couple',
+        src: './assets/mariage/couple.jpg',
+      },
+      {
+        id: 2,
+        name: 'couple',
+        src: './assets/mariage/couple2.jpg',
+      },
+      {
+        id: 3,
+        name: 'couple',
+        src: './assets/mariage/couple3.jpg',
+      },
+    ],
+    paysage: [
+      {
+        id: 1,
+        name: 'barque',
+        src: './assets/paysage/barque.jpg',
+      },
+      {
+        id: 2,
+        name: 'canal',
+        src: './assets/paysage/canal.jpg',
+      },
+      {
+        id: 3,
+        name: 'caverne',
+        src: './assets/paysage/caverne.jpg',
+      },
+      {
+        id: 4,
+        name: 'château',
+        src: './assets/paysage/chateau.jpg',
+      },
+      {
+        id: 5,
+        name: 'église',
+        src: './assets/paysage/eglise.jpg',
+      },
+      {
+        id: 6,
+        name: 'fleuve',
+        src: './assets/paysage/fleuve.jpg',
+      },
+      {
+        id: 7,
+        name: 'fleuve',
+        src: './assets/paysage/fleuve2.jpg',
+      },
+      {
+        id: 8,
+        name: 'fleuve',
+        src: './assets/paysage/fleuve3.jpg',
+      },
+      {
+        id: 9,
+        name: 'fleuve',
+        src: './assets/paysage/fleuve4.jpg',
+      },
+      {
+        id: 10,
+        name: 'hall',
+        src: './assets/paysage/hall.jpg',
+      },
+      {
+        id: 11,
+        name: 'parc',
+        src: './assets/paysage/parc.jpg',
+      },
+      {
+        id: 12,
+        name: 'parc',
+        src: './assets/paysage/parc.jpg',
+      },
+      {
+        id: 13,
+        name: 'passants',
+        src: './assets/paysage/passants.jpg',
+      },
+      {
+        id: 14,
+        name: 'passants',
+        src: './assets/paysage/passants2.jpg',
+      },
+      {
+        id: 15,
+        name: 'rue',
+        src: './assets/paysage/rue.jpg',
+      },
+      {
+        id: 16,
+        name: 'rue',
+        src: './assets/paysage/rue2.jpg',
+      },
+      {
+        id: 17,
+        name: 'rue',
+        src: './assets/paysage/rue3.jpg',
+      },
+      {
+        id: 18,
+        name: 'rue',
+        src: './assets/paysage/rue4.jpg',
+      },
+      {
+        id: 18,
+        name: 'squat',
+        src: './assets/paysage/squat.jpg',
+      },
+    ],
+    pleineAction: [
+      {
+        id: 1,
+        name: 'femme',
+        src: './assets/pleine_action/femme.jpg',
+      },
+      {
+        id: 2,
+        name: 'femme',
+        src: './assets/pleine_action/femme2.jpg',
+      },
+      {
+        id: 3,
+        name: 'femme',
+        src: './assets/pleine_action/femme3.jpg',
+      },
+      {
+        id: 4,
+        name: 'femme',
+        src: './assets/pleine_action/femme4.jpg',
+      },
+      {
+        id: 5,
+        name: 'femme',
+        src: './assets/pleine_action/femme5.jpg',
+      },
+      {
+        id: 6,
+        name: 'homme',
+        src: './assets/pleine_action/homme.jpg',
+      },
+      {
+        id: 7,
+        name: 'homme',
+        src: './assets/pleine_action/homme2.jpg',
+      },
+      {
+        id: 8,
+        name: 'homme',
+        src: './assets/pleine_action/homme3.jpg',
+      },
+      {
+        id: 9,
+        name: 'homme',
+        src: './assets/pleine_action/homme4.jpg',
+      },
+    ],
+    portrait: [
+      {
+        id: 1,
+        name: 'bébé',
+        src: './assets/portrait/bebe.jpg',
+      },
+      {
+        id: 2,
+        name: 'bébé',
+        src: './assets/portrait/bebe2.jpg',
+      },
+      {
+        id: 3,
+        name: 'bébé',
+        src: './assets/portrait/bebe3.jpg',
+      },
+      {
+        id: 4,
+        name: 'bébé',
+        src: './assets/portrait/bebe4.jpg',
+      },
+      {
+        id: 5,
+        name: 'bébé',
+        src: './assets/portrait/bebe5.jpg',
+      },
+      {
+        id: 6,
+        name: 'bébé',
+        src: './assets/portrait/bebe6.jpg',
+      },
+      {
+        id: 7,
+        name: 'bébé',
+        src: './assets/portrait/bebe7.jpg',
+      },
+      {
+        id: 8,
+        name: 'bébé',
+        src: './assets/portrait/bebe8.jpg',
+      },
+      {
+        id: 9,
+        name: 'bébé',
+        src: './assets/portrait/bebe9.jpg',
+      },
+      {
+        id: 10,
+        name: 'couple',
+        src: './assets/portrait/couple.jpg',
+      },
+      {
+        id: 11,
+        name: 'couple',
+        src: './assets/portrait/couple2.jpg',
+      },
+      {
+        id: 12,
+        name: 'couple',
+        src: './assets/portrait/couple3.jpg',
+      },
+      {
+        id: 13,
+        name: 'femme',
+        src: './assets/portrait/femme.jpg',
+      },
+      {
+        id: 14,
+        name: 'femme',
+        src: './assets/portrait/femme2.jpg',
+      },
+      {
+        id: 15,
+        name: 'homme',
+        src: './assets/portrait/homme.jpg',
+      },
+      {
+        id: 16,
+        name: 'homme',
+        src: './assets/portrait/homme2.jpg',
+      },
+    ],
   },
 ];
